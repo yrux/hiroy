65fc3084f3c50ff96f4831f0aee9a1d367ed9e5d
Yrux <yruxwork@gmail.com>
2021-12-08 00:31:10 +0500

    Initial commit

---
+++ connection.php
@@ -0,0 +1,8 @@
+<?php
+$databaseHost = 'localhost';
+$databaseName = 'rubarant_hiroy';
+$databaseUsername = 'rubarant_hiroy';
+$databasePassword = 'Free827193.';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+?>
